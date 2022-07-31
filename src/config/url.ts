6163a429a689a69cbf4c5bd3c06c7987e0d57602
generalkhun <khun2551@hotmail.com>
2022-07-31 09:57:53 +0700

    Add hard code env

--- src/config/url.ts
+++ src/config/url.ts
@@ -1,1 +1,3 @@
-export const WEB_SOCKET_ENDPOINT = process.env.WEB_SOCKET_ENDPOINT
+//export const WEB_SOCKET_ENDPOINT = process.env.WEB_SOCKET_ENDPOINT
+/** @todo figure out how to use vercel env variables (currently not working) */
+export const WEB_SOCKET_ENDPOINT ='wss://kth-service.webpubsub.azure.com/client/hubs/Hub?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ3c3M6Ly9rdGgtc2VydmljZS53ZWJwdWJzdWIuYXp1cmUuY29tL2NsaWVudC9odWJzL0h1YiIsImlhdCI6MTY1OTIwMTM0NCwiZXhwIjoxNjU5MjA0OTQ0fQ.C9zliNsTa2Zcfkp9zj4nTLvkod2Gwo2jbkjWRup8Ylk'
